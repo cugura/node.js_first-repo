0da0b90b5965c7e7d4fab2c2f586134d7584bbdc
Ivan Cugura <icugura@gmail.com>
2023-06-23 14:42:31 +0200

    prva promjena

---
+++ 1-intro.js
@@ -0,0 +1,16 @@
+const amount = 12
+
+if(amount < 10){
+    console.log('small number')
+} else {
+    console.log('large number')
+}
+
+console.log(`hey it's my first node app!!!!!`)
+
+console.log('sada' + __filename)
+
+setInterval(()=>{
+    console.log('hello world')
+}, 1000)
+
